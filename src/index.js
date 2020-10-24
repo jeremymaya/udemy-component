ce8d14490856ab49bf668f2a7a6854e75f84a4f2
Kyungrae Kim <kyungraekim@outlook.com>
2020-10-24 09:12:23 -0700

    passing multiple props with faker completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jeremiah" />
+            <CommentDetail
+                avatar={faker.image.avatar()}
+                author={faker.internet.userName()}
+                timeAgo="Today at 6:00PM"
+                content={faker.random.words()}
+            />
+            <CommentDetail
+                avatar={faker.image.avatar()}
+                author={faker.internet.userName()}
+                timeAgo="Today at 2:00PM"
+                content={faker.random.words()}
+            />
+            <CommentDetail
+                avatar={faker.image.avatar()}
+                author={faker.internet.userName()}
+                timeAgo="yesterday at 6:00PM"
+                content={faker.random.words()}
+            />
         </div>
     );
 };
